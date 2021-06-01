02d71440f489993ee65b161f15c81d47db45280d
annecchini <fernando.void@gmail.com>
2021-06-01 11:55:14 -0300

    ajustes no AppServiceProvider do heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Prevenir erros ao usar asset() no heroku
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
+
+        //Prevenir erros de column size em migrations no heroku
+        Schema::defaultStringLength(191);
+
     }
 }
